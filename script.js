bba68bb7e9a8235f0ad0f03e4c2b37c179312f6a
Ashir Imran <ashirimi1019@gmail.com>
2025-02-17 18:10:51 -0500

    Added Google Analytics and fixed tracking issues

--- script.js
+++ script.js
@@ -13,17 +13,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
     window.addEventListener("scroll", revealSections);
     revealSections(); // Initial call
-
-    // Google Analytics Integration
-    let script = document.createElement("script");
-    script.setAttribute("async", "");
-    script.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX");
-    document.head.appendChild(script);
-
-    script.onload = function() {
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('js', new Date());
-        gtag('config', 'G-XXXXXXX');
-    };
 });
